0159ad4ea33febd1c84658c7e8dfcc17edab5db5
thota-geethika <geetika.sree@m2pfintech.com>
2023-03-29 12:35:05 +0530

    Add. Added the testcase for Addition.

--- src/main/java/com/m2p/ArithmeticOperationsTDD.java
+++ src/main/java/com/m2p/ArithmeticOperationsTDD.java
@@ -1,4 +1,8 @@
 package com.m2p;
 
 public class ArithmeticOperationsTDD {
+    public int add(int firstNumber, int secondNumber) {
+
+        return firstNumber + secondNumber;
+    }
 }
--- src/test/java/com/m2p/ArithmeticOperationsTDDTest.java
+++ src/test/java/com/m2p/ArithmeticOperationsTDDTest.java
@@ -1,6 +1,25 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ArithmeticOperationsTDDTest {
 
     ArithmeticOperationsTDD arithmeticObject = new ArithmeticOperationsTDD();
+
+    @Test
+    // Test case For addition
+    void toReturnNineWhenFourIsAddedToFive()
+    {
+        //AAA format
+        //Arrange
+        int expectedValue = 9;
+
+        //Act
+        int actualValue = arithmeticObject.add(4,5);
+
+        //Assert
+        assertEquals(expectedValue,actualValue);
+    }
 }
